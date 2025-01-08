e33a5df9e54dea0ec35c4250cb3bcfef0a2da706
wennanyang <341028yang@gmail.com>
2025-01-08 15:42:45 +0000

    complete non local attention

---
+++ models/attention.py
@@ -0,0 +1,168 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import einops
+from models import register
+class BasicBlock(nn.Sequential):
+    def __init__(
+        self, conv, in_channels, out_channels, kernel_size, stride=1, bias=True,
+        bn=False, act=nn.PReLU()):
+
+        m = [conv(in_channels, out_channels, kernel_size, bias=bias)]
+        if bn:
+            m.append(nn.BatchNorm2d(out_channels))
+        if act is not None:
+            m.append(act)
+
+        super(BasicBlock, self).__init__(*m)
+def default_conv(in_channels, out_channels, kernel_size,stride=1, bias=True):
+    return nn.Conv2d(
+        in_channels, out_channels, kernel_size,
+        padding=(kernel_size//2),stride=stride, bias=bias)
+@register("NLA")
+class NonLocalAttention(nn.Module):
+    def __init__(self, in_dim=3, K = 3, dims=[6, 9], conv=default_conv):
+        super(NonLocalAttention, self).__init__()
+        self.conv1 = nn.Conv2d(in_dim, dims[0], 3, 2, 1, bias=False)
+        self.conv2 = nn.Conv2d(dims[0], dims[1], 3, 2, 1, bias=False)
+        self.x1 = None
+        self.x2 = None
+        self.x3 = None
+        self.K = K
+        self.q = None
+        self.v = None
+        # 第3层使用的proj
+        self.proj_q3 = nn.Conv2d(dims[1], dims[1], 1, 1, 0, bias=False)
+        self.proj_k3 = nn.Conv2d(dims[1], dims[1], 1, 1, 0, bias=False)
+        # self.proj_v3 = nn.Conv2d(dims[1], dims[1], 1, 1, 0, bias=False)
+        # 第2层使用的proj
+        self.proj_q2 = nn.Conv2d(dims[1], dims[1], 1, 1, 0, bias=False)
+        self.proj_k2 = nn.Conv2d(dims[1], dims[1], 1, 1, 0, bias=False)
+        # self.proj_v2 = nn.Conv2d(dims[1], dims[1], 1, 1, 0, bias=False)
+        # 第1层使用的proj
+        self.proj_q1 = nn.Conv2d(dims[1], dims[1], 1, 1, 0, bias=False)
+        self.proj_k1 = nn.Conv2d(dims[1], dims[1], 1, 1, 0, bias=False)
+        # self.proj_v1 = nn.Conv2d(dims[1], dims[1], 1, 1, 0, bias=False)
+
+        self.scale = dims[1] ** -0.5
+    def expand_indices(self, topK_indices, scale, now_shape):
+        '''
+        topK_indices: [B, C, H*W], 每个点topK个近似的点
+        scale : int, 放大倍数
+        now_shape : [H, W], 放大前的shape
+        '''
+        B, N, K = topK_indices.shape
+        H, W = now_shape
+        # 假设某个点的序列号为n1，则扩大scale倍后的序列号n2是这样计算的
+        # n1 % W = i, n - i = j
+        # n2 = j * scale * scale + i * scale
+        new_topK = torch.zeros_like(topK_indices)
+        for k in range(K):
+            i = topK_indices[:, :, k] % W
+            j = topK_indices[:, :, k] - i
+            new_topK[:, :, k] = j * scale * scale + i * scale
+        # 先把水平方向上的重复一遍
+        new_topK = new_topK.repeat_interleave(scale, dim=2)
+        for s in range(scale):
+            new_topK = new_topK[:, : s : (s + 1) * scale] + s
+        # 得到了每个点第一行的排列，将每一行重复scale次
+        # 就得到了每个点扩展成的(scale, scale)的大小
+        # 无需考虑顺序问题，因为索引大小表示前后，计算出值排序即可
+        new_topK = new_topK.repeat(1, 1, scale)
+        for s in range(scale):
+            new_topK = new_topK[:, :, s : (s + 1) * scale * K] + scale * W
+        new_topK, _ = torch.sort(new_topK, dim=2)
+        # 接着需要注意顺序，先每个点重复scale次，接着分组重复scale次
+        new_topK = new_topK.repeat_interleave(scale, dim=1)
+        # 每一行有W * scale个元素，所以按照这个分割每一行
+        split_tensors = torch.split(new_topK, W * scale, dim=2)
+        # 将每一行重复scale次
+        repeated_segments = [segment.repeat(1, scale, 1) for segment in split_tensors]
+        # result = [B, N*scale*scale, K*scale*scale]
+        result = torch.cat(repeated_segments, dim=1)
+        return result
+    def attention(self, x, indices, layer=2):
+        B, C, H, W = x.shape
+        if layer == 2:
+            q = self.proj_q2(x).reshape(B, C, H*W)
+            k = self.proj_k2(x).reshape(B, C, H*W)
+            # v = self.proj_v2(x).reshape(B, C, H*W)
+        else :
+            q = self.proj_q1(x).reshape(B, C, H*W)
+            k = self.proj_k1(x).reshape(B, C, H*W)
+            # v = self.proj_v1(x).reshape(B, C, H*W)
+            self.q = q
+            self.k = k
+            # self.v = v
+        k = torch.gather(k, dim=2, index=indices)
+        # 将q, k, v转置，通道放在最后面
+        # q=[B, N, C], k,v =[B, N, K, C]
+        q = q.permute(0, 2, 1)
+        k = k.permute(0, 2, 3, 1)
+        # scores = [B, N, K]
+        attention_scores = torch.einsum('bnc,bnkc->bnk', q, k)
+        d_k = C
+        attention_scores = attention_scores / (d_k ** 0.5)
+        attention_weights = F.softmax(attention_scores, dim=2)
+        return attention_weights
+    
+    def non_local_attention(self):
+        '''
+        第一阶段 在最小的形状上
+        '''
+        x = self.x3
+        B, C, H, W = x.shape
+        q = self.proj_q3(x).reshape(B, C, H*W)
+        k = self.proj_k3(x).reshape(B, C, H*W)
+        v = self.proj_v3(x).reshape(B, C, H*W)
+        attention_3 = torch.einsum('b c m, b c n -> b m n', q, k)
+        attention_3 = F.softmax(attention_3, dim=2)
+        print(f"attention.shape = {attention_3.shape}")
+        # 取出前self.K个most similar的
+        _, topK_indices_3 = torch.topk(attention_3, self.K, dim=2)
+        print(f"topK_indices = {topK_indices_3.shape}")
+        '''
+        第二阶段
+        '''
+        # 将indices扩展
+        topK_indices_3_expand = self.expand_indices(topK_indices_3, self.scale, (H, W))
+        # 利用indices采样
+        attention2 = self.attention(self.x2, topK_indices_3_expand, 2)
+        _, topK_indices_2 = torch.topk(attention2, self.K, dim=2)
+        '''
+        第三阶段
+        '''
+        topK_indices_2_expand = self.expand_indices(topK_indices_2, self.scale, 
+                                            (self.x2.shape[-2], self.x2.shape[-1]))
+        attention1 = self.attention(self.x1, topK_indices_2_expand, 1)
+        '''
+        最后输出阶段
+        '''
+        # 得到前K个most similar
+        topK_indices = torch.topk(attention1, self.K, dim=2)
+        Q = self.q
+        K = torch.gather(self.k, dim=2, index=topK_indices)
+        V = K
+        Q = Q.permute(0, 2, 1) # [B, N, C]
+        K = K.permute(0, 2, 3, 1) # [B, N, K, C]
+        V = V.permute(0, 2, 3, 1) # [B, N, k, C]
+        attention_scores = torch.einsum('bnc,bnkc->bnk', Q, K)
+        d_k = self.x1.shape[1]
+        attention_scores = attention_scores / (d_k ** 0.5)
+        attention_weights = F.softmax(attention_scores, dim=2) # [B, N, K]
+        output = torch.einsum('bnk,bnkc->bnc', attention_weights, V) # [B, N, C]
+        return output
+
+    def forward(self, x):
+        self.x1 = x
+        self.x2 = self.conv1(x)
+        self.x3 = self.conv2(self.x2)
+        # print(f"self.x1.shape = {self.x1.shape}")
+        # print(f"self.x2.shape = {self.x2.shape}")
+        # print(f"self.x3.shape = {self.x3.shape}")
+
+        output = self.non_local_attention()
+        return output
+        
+        
+
---
+++ nlatest.py
@@ -0,0 +1,16 @@
+from models.attention import NonLocalAttention
+
+import torch 
+
+def main():
+    device = "cuda:0"
+    batch = torch.ones((16, 3, 128, 128), device=device, dtype=torch.float)
+    model = NonLocalAttention()
+    model.to(device=device)
+    model(batch)
+    tensor = torch.tensor([[1, 2], [3, 4]])
+    tensor = tensor.repeat(1, 2)
+    print(tensor)
+
+if __name__ == '__main__':
+    main()
